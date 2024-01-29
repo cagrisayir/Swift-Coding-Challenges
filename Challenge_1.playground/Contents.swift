02a070e234accf191b0affc53655936cf625dbd9
Omer Cagri Sayir <omercagris@gmail.com>
2024-01-29 19:09:12 +0300

    solution of challenge 1

--- Challenge_1.playground/Contents.swift
+++ Challenge_1.playground/Contents.swift
@@ -1,3 +1,25 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func challenge1(input: String) -> Bool {
+    var usedLetters: [Character] = []
+    for letter in input {
+        if usedLetters.contains(letter) {
+            return false
+        } else {
+            usedLetters.append(letter)
+        }
+    }
+
+    return true
+}
+
+challenge1(input: "No duplicates")
+challenge1(input: "Hello, World")
+
+// Second way with Set
+func challenge1b(input: String) -> Bool {
+    return Set(input).count == input.count
+}
+
+challenge1b(input: "No duplicates")
+challenge1b(input: "Hello, World")
